d33fce5bca1673bc23f75eb54b92b43a491f6af0
currysaucee <jonathanau1999@gmail.com>
2017-02-03 13:33:31 +0800

    CA2
    First edit for trying of media query of Index page

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 
+   Lakeland Reeds Bed and Breakfast menu toggle script 
+   Filename: script.js
+   HTML5 and CSS3 Illustrated Unit I, Lessons
+ */
+
+var navButton = document.querySelector(".menudropdown img");
+if (navButton.addEventListener) {
+   navButton.addEventListener("click", function() {
+      var nav = document.querySelector(".sitenavigation ul");
+      if (nav.style.display === "block") {
+         nav.style.display = "none";
+      } else {
+         nav.style.display = "block";
+      }
+   }, false);
+}
+)
