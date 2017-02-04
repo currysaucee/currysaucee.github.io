ab5ffb2b9e56da6c8663cb91e8ebcdf1db62f81b
currysaucee <jonathanau1999@gmail.com>
2017-02-04 18:37:33 +0800

    Update for Mobile

--- script.js
+++ script.js
@@ -4,15 +4,14 @@
    HTML5 and CSS3 Illustrated Unit I, Lessons
  */
 
-var navButton = document.querySelector(".menudropdown img");
+var navButton = document.querySelector(".navigation-menu-button img");
 if (navButton.addEventListener) {
    navButton.addEventListener("click", function() {
       var nav = document.querySelector(".sitenavigation ul");
       if (nav.style.display === "block") {
-         nav.style.display = "none";
+         nav.style.display = "";
       } else {
          nav.style.display = "block";
       }
    }, false);
 }
-)
